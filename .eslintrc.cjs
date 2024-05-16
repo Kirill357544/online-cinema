298b5b5f54a1cf4b812c20a3715b92381d357f4e
Kirill Lalov <kirill.lalov@artezio.com>
2024-05-16 17:11:25 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2023: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: 'error',
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+      imports: 'always-multiline',
+      objects: 'always-multiline',
+    }],
+  },
+}
