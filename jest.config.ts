13aa9789a1077bc01e24ad8f8a90df7ab2656f72
Lalov Kirill <lalovkir@gmail.com>
2024-05-22 22:29:10 +0300

    Добавлена конфигурация JEST

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  clearMocks: true,
+  verbose: true,
+  testEnvironment: 'jsdom',
+  rootDir: './',
+  testMatch: [
+    '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)',
+  ],
+  // setupFilesAfterEnv: ['<rootDir>config/jest/jest-setup.ts'],
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy',
+    '@app/(.*)': '<rootDir>/src/app/$1',
+    '@pages/(.*)': '<rootDir>/src/pages/$1',
+    '@widgets/(.*)': '<rootDir>/src/widgets/$1',
+    '@features/(.*)': '<rootDir>/src/features/$1',
+    '@entities/(.*)': '<rootDir>/src/entities/$1',
+    '@shared/(.*)': '<rootDir>/src/shared/$1',
+  },
+};
+
+export default config;
