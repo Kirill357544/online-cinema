679c4c423479e4744de0b91ffca34c9fb3a14dcb
Lalov Kirill <lalovkir@gmail.com>
2024-05-25 21:50:04 +0300

    Добавлен шрифты

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+
+import './styles/styles.css';
+
+export const App: FC = () => {
+  return (
+    <>
+      <h1 className='one'>Online-cinema</h1>
+      <h1 className='two'>Online-cinema</h1>
+    </>
+  );
+};
