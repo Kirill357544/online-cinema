9c52971521cdb672cd1d32bcaf09a30dfe7e95f5
Kirill_Lalov <50410587+Kirill357544@users.noreply.github.com>
2024-05-28 17:27:56 +0300

    Добавлен сброс стандартных стилей (#13)
    Co-authored-by: Kirill Lalov <kirill.lalov@artezio.com>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,11 @@
 import { FC } from 'react';
 
-import './styles/styles.css';
-import { Spinner } from '@shared/ui/Spinner';
+import '@shared/styles/styles.css';
 
 export const App: FC = () => {
   return (
     <>
-      <h1 className='one'>Online-cinema</h1>
       <h1 className='two'>Online-cinema</h1>
-      <Spinner />
     </>
   );
 };
