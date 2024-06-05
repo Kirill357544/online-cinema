fbfa16c21cf107d7d20605ec4804f8557d0bc058
Lalov Kirill <lalovkir@gmail.com>
2024-06-05 21:47:38 +0300

    Добавлена темная тема

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,16 @@ import { FC, useState } from 'react';
 import { Rating } from '@shared/ui/Rating';
 
 import '@shared/styles/styles.css';
+import { Button } from '@shared/ui/Button';
 
 export const App: FC = () => {
   const [rating, setRating] = useState(5);
 
   return (
-    <>
+    <div className='light'>
       <Rating size={10} rating={rating} onChange={setRating} />
       <h1>{rating}</h1>
-    </>
+      <Button>test</Button>
+    </div>
   );
 };
